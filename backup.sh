f3ccdefa98f9dd7069697e254b056b8d1c338710
Foxi <293639+noxifoxi@users.noreply.github.com>
2020-09-16 15:04:45 +0200

    Removed debugging added switch for latest link
    Linking the latest backup in "<backup path>/latest/backup.7z", for possible syncing purposes.

--- backup.sh
+++ backup.sh
@@ -9,23 +9,21 @@ source_path="/home/minecraft/server/*"
 backup_path="/media/backupdrive/mc-backup/"
 screen_name="minecraft" # name of the screen the server is running
 compression=9 # 0-9
-debug=0 # 0-1
+create_latest=1 # 0-1
 
 
 # ============= #
 #  Anouncement  #
 # ============= #
-if [ $debug == 0 ]; then
-	screen -S $screen_name -p 0 -X stuff "say §cBackup in $sleep seconds...^M"
-	if [ $stop_server == 1 ]; then
-		screen -S $screen_name -p 0 -X stuff "say §eThe server will shutdown for ~5 minutes!^M"
-	else
-		screen -S $screen_name -p 0 -X stuff "say §eThe server will lag!^M"
-	fi
-
-	# wait for the configured amount of seconds
-	sleep $sleep
+screen -S $screen_name -p 0 -X stuff "say §cBackup in $sleep seconds...^M"
+if [ $stop_server == 1 ]; then
+	screen -S $screen_name -p 0 -X stuff "say §eThe server will shutdown for ~5 minutes!^M"
+else
+	screen -S $screen_name -p 0 -X stuff "say §eThe server will lag!^M"
 fi
+
+# wait for the configured amount of seconds
+sleep $sleep
 
 
 # ==================== #
@@ -40,7 +38,6 @@ if [ $stop_server == 0 ]; then
 fi
 
 if [ $stop_server == 1 ]; then
-	if [ $debug == 1 ]; then echo "Stopping server..."; fi
 	screen -S $screen_name -p 0 -X stuff "stop^M"
 
 	# give server shutdown and world saving a bit of time
@@ -51,12 +48,10 @@ fi
 # ======== #
 #  Backup  #
 # ======== #
-if [ $debug == 1 ]; then
-	echo "backup in progress..."
-	7z a -mx=$compression -y "$backup_path$(date +%Y-%m-%d-%H%M%S).7z" $source_path
-else
-	# silent
-	7z a -mx=$compression -bsp0 -bso0 -y "$backup_path$(date +%Y-%m-%d-%H%M%S).7z" $source_path > /dev/null
+filename=$backup_path"$(date +%Y-%m-%d-%H%M%S).7z"
+7z a -mx=$compression -bsp0 -bso0 -y $filename $source_path > /dev/null
+if [ $create_latest == 1 ]; then
+	ln -f $filename $backup_path"latest/backup.7z"
 fi
 
 
@@ -66,10 +61,7 @@ fi
 if [ $stop_server == 1 ]; then
 	# starting the server
 	screen -S $screen_name -p 0 -X stuff "./start.sh^M"
-	if [ $debug == 1 ]; then echo "Starting server..."; fi
 else
 	# enable auto saving again
 	screen -S $screen_name -p 0 -X stuff "save-on^Msay §aBackup done!^M"
 fi
-
-if [ $debug == 1 ]; then echo "backup done."; fi
